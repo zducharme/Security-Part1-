a2462369f8db5333468e84d26cdff9e5c6c88508
zducharme <43576501+zducharme@users.noreply.github.com>
2019-11-29 17:11:56 -0500

    Create SecurityAccessAudit_DailyUsers.sh

---
+++ SecurityAccessAudit_DailyUsers.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Create originating directory and change active directories.
+touch ~/11-8-2019/SecurityScripts_#1/securityaudits
+cd ~/11-8-2019/SecurityScripts_#1/securityaudits
+
+last > DailyLogins_TEMP.txt
+
+# END
