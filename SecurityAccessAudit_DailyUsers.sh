f931856e2693429f650753b8efe75a1f5e0a68f5
zducharme <43576501+zducharme@users.noreply.github.com>
2019-11-29 18:33:25 -0500

    Update SecurityAccessAudit_DailyUsers.sh

--- SecurityAccessAudit_DailyUsers.sh
+++ SecurityAccessAudit_DailyUsers.sh
@@ -1,5 +1,6 @@
-# Zachary Ducharme 11/8/19
 #!/bin/bash
+
+# Zachary Ducharme 11/8/19
 
 # Create originating directory and change active directories.
 touch ~/11-8-2019/SecurityScripts_#1/securityaudits
