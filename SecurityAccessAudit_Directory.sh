1f44d2082baabfdb76df1001f95c14b33b80f8c2
zducharme <43576501+zducharme@users.noreply.github.com>
2019-11-29 17:14:00 -0500

    Create SecurityAccessAudit_Directory.sh

---
+++ SecurityAccessAudit_Directory.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Compare Directory Changes
+
+cronjob -e 0 24 0 0 0 cp /var/log/messages /directorychanges/log/messages
+diff -ur /var/log/messages /directorychangges/log/messages
+cat /var/log/messages /directorychanges/log/messages > /directorychanges/output/SecurityAudit_DetectDirectory.log
+
+# END
